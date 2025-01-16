13e50c6f99f53dbaec9d9a200ca5f8d83f3ea0d3
MRegiAgustianH <muhmmad.regi@gmail.com>
2025-01-16 12:18:48 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,45 @@
+<x-app-layout>
+    <x-slot name="header">
+        <img src="/images/banner1.png" alt="">
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    
+                    <h2  class="text-xl text-center font-bold mb-4">INTRODUCTION</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo recusandae atque fuga ratione consequuntur suscipit voluptatem rerum praesentium, iure exercitationem mollitia quis repudiandae perferendis fugit, quia ipsa porro ex iste.
+                        Omnis laboriosam, ducimus aperiam illum veniam consequuntur in autem eum necessitatibus dolorem voluptates repellendus, non temporibus, aut sed dolorum sequi nemo ut sit suscipit cumque dicta laudantium numquam? Eveniet, esse.
+                        Incidunt dolor eaque non, beatae voluptatibus ipsam consequuntur id necessitatibus sequi sunt numquam dolorem corrupti a excepturi minima illum eligendi, in enim! Ex impedit iste nobis placeat, alias atque delectus!
+                        Quo dicta at placeat error aut est vitae enim facere, consequatur inventore debitis, suscipit cupiditate voluptatibus, laudantium sunt totam veniam ab ea obcaecati. Sit inventore placeat eveniet! Laborum, cupiditate veniam?
+                        Ab magni unde ad maiores eos nulla fugiat soluta saepe necessitatibus. Earum ea quidem expedita rerum sunt nulla debitis, eum ipsam illo hic quis fugiat eveniet omnis sed repellat magni.  
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet quia at voluptas maiores. Ipsam quaerat cumque impedit tempora, temporibus velit corrupti eos quo, obcaecati eaque unde ad voluptatibus ratione.
+                        Sint quod error similique reiciendis sapiente nemo earum unde, minus cumque ad quam dolorem dolores provident. Nesciunt qui provident suscipit quam, eaque rem iusto delectus obcaecati architecto totam at voluptates.
+                        In fugit libero nam, ipsam atque harum nobis beatae ab blanditiis repellendus soluta vitae odit veritatis consequuntur, vero sint sed laudantium! Provident sapiente reprehenderit magni nemo veniam! Error, laborum inventore.
+                        Nobis similique, praesentium architecto enim nemo nesciunt, omnis quasi est esse, repellat non! Autem aperiam maiores dolores illo sequi asperiores, voluptatum optio enim natus eum, atque repellendus aut dolorum consequuntur?
+                        Nobis possimus magnam porro labore fugit vitae eius tempora quis id ad, voluptas quas provident. Unde minus ex nihil earum quibusdam aliquam atque, fugit, nisi non debitis aut velit dolorem?
+                        Exercitationem eum labore non numquam obcaecati impedit eos quasi consequatur soluta dolorem voluptates cum, doloribus saepe, minus, maxime eaque modi accusantium error suscipit rerum! Dolore quaerat ea pariatur distinctio perferendis.
+                        Dolor error, sint illo unde necessitatibus voluptas in? Quaerat illum voluptas vitae accusantium laudantium eligendi nam natus beatae recusandae harum esse repudiandae laboriosam, expedita tenetur nostrum explicabo porro, quam quis.
+                        Expedita ipsam aspernatur deserunt, error aliquam neque, eligendi molestias ipsum atque eos animi fuga reprehenderit? Excepturi ullam placeat cum veniam ipsam. Maiores atque, delectus molestiae obcaecati id totam quo officia.
+                        Sint eum, deserunt tempore veniam voluptatem sunt corporis delectus dolores reprehenderit voluptatum. Pariatur tempore nisi consectetur architecto quia impedit porro inventore voluptatum quasi maxime provident voluptas, perspiciatis magnam dolorum hic.
+                        Sit, ratione, culpa dolorum illo eum eos dignissimos vel velit dolores molestiae expedita quam reiciendis deleniti sint tempora quaerat ipsum blanditiis commodi neque aut porro placeat odio voluptates. Minima, eum?
+                        Totam perferendis, dignissimos praesentium ratione in eius pariatur blanditiis reprehenderit animi quis repellat voluptatum ipsum aspernatur quisquam asperiores nam dolore dolorum atque illo eveniet earum. Nesciunt, alias? Accusantium, expedita veniam.
+                        Accusamus ab suscipit nemo illo nam illum maiores voluptatum ad unde totam quo quas inventore eaque, velit voluptas eveniet in eum architecto, quia natus. Voluptatem nobis tempora odio molestiae quos?
+                        Aspernatur amet nisi perspiciatis ex esse non quisquam. Eveniet porro veritatis, quaerat alias tenetur corrupti architecto excepturi commodi, unde qui, quisquam quasi optio mollitia. Quibusdam minus deleniti sequi harum quisquam?
+                        Vel aspernatur illum, quas molestias harum delectus. Debitis, quae! Explicabo, beatae aut pariatur libero consequuntur quas dolore repudiandae porro maiores accusantium qui consectetur eveniet officia similique natus amet ut fuga.
+                        Possimus laborum qui, sint eos dolores harum ratione asperiores assumenda sit ducimus rem quos quisquam pariatur perspiciatis dolor consequatur esse nisi? Beatae minus unde atque nobis earum ducimus cumque itaque?
+                        At quidem porro velit saepe. Asperiores ipsum qui deserunt a blanditiis sequi expedita repudiandae eos quis debitis rem dolor nesciunt et alias quos dolorum, dignissimos nobis maiores esse sed facilis?
+                        Accusamus quibusdam ut ab deleniti, eaque vel optio. Sint soluta mollitia repellendus necessitatibus corporis, vitae reiciendis excepturi eveniet deserunt ex tempore consequatur fugiat qui enim? Fugiat velit dicta cum minima!
+                        Inventore architecto at nam eligendi eveniet facere, itaque aliquid illum nostrum libero voluptas ipsa cum error temporibus, totam labore ea ad. Itaque facere porro amet aliquid maxime corporis fuga eligendi.
+                        In adipisci veniam iure sunt, laborum id voluptatem quasi incidunt aliquam eveniet architecto doloremque, aut itaque odit mollitia animi nesciunt? Asperiores, aperiam expedita perspiciatis commodi veritatis quasi fuga laudantium sed!
+                        Ex natus fugiat accusantium, quam sit sunt cupiditate error. Ab accusamus illo atque ipsa nesciunt nobis error ea numquam iusto harum officia fuga rerum, ratione beatae accusantium animi tempore suscipit.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
